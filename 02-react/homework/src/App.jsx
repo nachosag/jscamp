e5f0ac5d901ac96e6c8698133a847b41e26490d1
nachosag <ignacioborlenghi@gmail.com>
2025-12-30 10:51:42 -0300

    Init DevJobs web with React

---
+++ 02-react/homework/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Header } from './components/Header'
+import { Hero } from './components/Hero'
+import { WhyUs } from './components/WhyUs'
+import { Footer } from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      {/* <WhyUs /> */}
+      {/* <Footer /> */}
+    </>
+  )
+}
+
+export default App
